19b941a798bfa2820260244ec0d2c8dc9d9f9fa1
Lucas <lucassantos.santos90@hotmail.com>
2023-02-08 15:07:10 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 
 import './App.css';
+import HelloWorld from './components/HelloWorld'; 
 
 function App() {
+
+  function Sum(a, b) {  
+    return a + b;
+  }
+
+const name = 'Lucas';
+
+const URL = 'https://www.google.com.br/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png/150';
+
   return (
     <div className="App">
       <h1>Olá React!</h1>
-      <p>Meu primeiro App</p>
+      <p>Olá, {name}</p>
+      <p>Soma: {Sum(1, 2)}</p>
+      <img src={URL} alt="lucas"/>
+      <HelloWorld />
     </div>
   );
 }
 
 export default App;
+
+ 
