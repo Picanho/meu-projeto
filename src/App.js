6674928d5c46ba2a44521ce7619c9f4b3f4ced39
Lucas <lucassantos.santos90@hotmail.com>
2023-02-14 22:16:36 -0300

    Eventos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-
 import './App.css';
-import Frase from './components/frase';
-import List from './components/List';
-import Pessoa from './components/Pessoa';
-import SayMyName from './components/SayMyName';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
 
-  const nome = "Maria";
-
   return (
     <div className="App">
-      <h1>Testando css</h1>
-      <Frase />
-      <Frase />
-      <SayMyName name="Lucas" />
-      <SayMyName name="Jean" />
-      <SayMyName name={nome} />
-      <Pessoa nome="João" idade="25" profissao="Programador" foto="https://avatars.githubusercontent.com/u/5624255?v=4/" />
-     <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero='1' />
+      <Evento numero='2' />
+      <Form />
     </div>
   );
 }
