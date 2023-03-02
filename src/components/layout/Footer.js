a19a6a54febf735ecd5c1aa128af10a6b6f1cfbb
Lucas <lucassantos.santos90@hotmail.com>
2023-03-02 13:55:00 -0300

    React Icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,16 @@
+import {FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css';
 import React from "react";
 function Footer() {
   return (
     <footer>
-      Rodape
-    </footer>
+      <ul className={styles.social_list}>
+      <li><FaFacebook /></li>
+      <li><FaInstagram /></li>
+      <li><FaLinkedin /></li>
+      </ul>
+      <p>Nosso rodape</p>
+      </footer>
   );
 }
 
